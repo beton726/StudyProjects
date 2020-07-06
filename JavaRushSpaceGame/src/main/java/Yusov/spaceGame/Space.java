6c2b3d191b27deb3e9440e6e199a691e41700ae6
beton726 <909043377fas>
2020-07-06 15:32:42 +0300

    Fill class Space

--- JavaRushSpaceGame/src/main/java/Yusov/spaceGame/Space.java
+++ JavaRushSpaceGame/src/main/java/Yusov/spaceGame/Space.java
@@ -1,13 +1,56 @@
 package Yusov.spaceGame;
 
-import sun.java2d.pipe.SpanShapeRenderer;
+import Yusov.spaceGame.flyObjects.SpaceShip;
+import Yusov.spaceGame.flyObjects.Ufo;
+import Yusov.spaceGame.weapons.Bomb;
+import Yusov.spaceGame.weapons.Rocket;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Space {
+
+    private int width;
+    private int height;
+    private SpaceShip ship;
+
+    private List<Ufo> ufos = new ArrayList<Ufo>();
+    private List<Rocket> rockets  = new ArrayList<Rocket>();
+    private List<Bomb> bombs  = new ArrayList<Bomb>();
+
+    public Space(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public void setShip(SpaceShip ship) {
+        this.ship = ship;
+    }
+
+    public SpaceShip getShip() {
+        return this.ship;
+    }
+
+    public int getWidth() {
+        return this.width;
+    }
+
+    public int getHeight() {
+        return this.height;
+    }
+
+    public List<Ufo> getUfos() {
+        return this.ufos;
+    }
+
+    public List<Rocket> getRockets() {
+        return this.rockets;
+    }
+
+    public List<Bomb> getBombs() {
+        return this.bombs;
+    }
+
     public static void main(String[] args) {
-        System.out.println(new Date().toString());
-        System.out.println((new Date()).toString());
     }
 }
