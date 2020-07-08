56372b2484f55ca63d0d2d61bc1d756cbf106455
beton726 <909043377fas>
2020-07-08 18:46:11 +0300

    Start project

--- SampleLoggingCode/src/main/java/src/yusov/Source.java
+++ SampleLoggingCode/src/main/java/src/yusov/Source.java
@@ -1,7 +1,22 @@
 package src.yusov;
 
+import java.io.IOException;
+import java.util.logging.*;
+
 public class Source {
-    public static void main(String[] args) {
+
+    private static final Logger logger = Logger.getLogger(Source.class.getName());
+
+    public static void main(String[] args) throws IOException {
+        // Чтение конфигурации
+        LogManager.getLogManager().readConfiguration();
+
+        Handler fileHandler = new FileHandler();
+        logger.setUseParentHandlers(false);     // отключает вывод в консоль
+//        logger.addHandler(fileHandler);
+
+        logger.info("info");
+
 
     }
 }
