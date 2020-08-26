0693c69f67e82135c4383ca00b73296312a25863
beton726 <909043377fas>
2020-08-26 14:37:10 +0300

    Tetris

--- JavaRushTetris/src/main/java/src/field/Field.java
+++ JavaRushTetris/src/main/java/src/field/Field.java
@@ -1,9 +1,14 @@
 package src.field;
 
+import src.game.Tetris;
+
+/* *
+ * Хранение данных о текущих занятых и свободных клетках на поле игры. (Поле)
+ * */
 public class Field {
 
-    private int width;
-    private int height;
+    private int width;  // Ширина
+    private int height; // Высота
 
     private int[][] matrix;
 
@@ -24,20 +29,82 @@ public class Field {
     public int[][] getMatrix() {
         return matrix;
     }
-
+    /**
+     * Метод печатает на экран текущее содержание матрицы
+     */
     public void print() {
+        //Создаем массив, куда будем "рисовать" текущее состояние игры
+        int[][] canvas = new int[height][width];
 
+        //Копируем "матрицу поля" в массив
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                canvas[i][j] = matrix[i][j];
+            }
+        }
+
+        //Копируем фигурку в массив, только непустые клетки
+        int left = Tetris.game.getFigure().getX();
+        int top = Tetris.game.getFigure().getY();
+        int[][] brickMatrix = Tetris.game.getFigure().getMatrix();
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (top + i >= height || left + j >= width) continue;
+                if (brickMatrix[i][j] == 1)
+                    canvas[top + i][left + j] = 2;
+            }
+        }
+
+        //Выводим "нарисованное" на экран, но начинаем с "границы кадра".
+        System.out.println("---------------------------------------------------------------------------\n");
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                int index = canvas[i][j];
+                if (index == 0)
+                    System.out.print(" . ");
+                else if (index == 1)
+                    System.out.print(" X ");
+                else if (index == 2)
+                    System.out.print(" X ");
+                else
+                    System.out.print("???");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println();
     }
 
     public void removeFullLines() {
-
+        for (int i = 0; i < height; i++) {
+            int count = 0;
+            for (int j = 0; j < width; j++) {
+                if(matrix[i][j] == 1)
+                    count++;
+            }
+            if(count == width)
+                matrix[i] = null;
+                matrix[i+1] = matrix[i];
+                matrix[i] = new int[width];
+        }
     }
-
+    /**
+     * Метод возвращает значение, которое содержится в матрице с координатами (x,y)
+     * Если координаты за пределами матрицы, метод возвращает null.
+     */
     public Integer getValue(int x, int y) {
-        return matrix[y][x];
+        if (x >= 0 && x < width && y >= 0 && y < height)
+            return matrix[y][x];
+        return null;
     }
-
+    /**
+     * Метод устанавливает переданное значение(value) в ячейку матрицы с координатами (x,y)
+     */
     public void setValue(int x, int y, int value) {
-        matrix[y][x] = value;
+        if (x >= 0 && x < width && y >= 0 && y < height)
+            matrix[y][x] = value;
     }
 }
--- JavaRushTetris/src/main/java/src/game/Tetris.java
+++ JavaRushTetris/src/main/java/src/game/Tetris.java
@@ -1,13 +1,25 @@
 package src.game;
-
+/* *
+ * Игра тетрис.
+ * */
 import src.field.Field;
 import src.figure.Figure;
+import src.figure.FigureFactory;
+
+import java.awt.event.KeyEvent;
 
 public class Tetris {
 
     public static Tetris game;
     private Field field;
     private Figure figure;
+
+    private boolean isGameOver;
+
+    public Tetris(int width, int heigth) {
+        field = new Field(width, heigth);
+        figure = null;
+    }
 
     public void setField(Field field) {
         this.field = field;
@@ -24,17 +36,77 @@ public class Tetris {
     public Figure getFigure() {
         return figure;
     }
+    /**
+     *  Основной цикл программы.
+     *  Тут происходят все важные действия
+     */
+    public void run() throws Exception
+    {
+        //Создаем объект "наблюдатель за клавиатурой" и стартуем его.
+        KeyboardObserver keyboardObserver = new KeyboardObserver();
+        keyboardObserver.start();
 
-    public void run() {
+        //выставляем начальное значение переменной "игра окончена" в ЛОЖЬ
+        isGameOver = false;
+        //создаем первую фигурку посередине сверху: x - половина ширины, y - 0.
+        figure = FigureFactory.createRandomFigure(field.getWidth() / 2, 0);
 
+        //пока игра не окончена
+        while (!isGameOver)
+        {
+            //"наблюдатель" содержит события о нажатии клавиш?
+            if (keyboardObserver.hasKeyEvents())
+            {
+                //получить самое первое событие из очереди
+                KeyEvent event = keyboardObserver.getEventFromTop();
+                //Если равно символу 'q' - выйти из игры.
+                if (event.getKeyChar() == 'q') return;
+                //Если "стрелка влево" - сдвинуть фигурку влево
+                if (event.getKeyCode() == KeyEvent.VK_LEFT)
+                    figure.left();
+                    //Если "стрелка вправо" - сдвинуть фигурку вправо
+                else if (event.getKeyCode() ==  KeyEvent.VK_RIGHT)
+                    figure.right();
+                    //Если  код клавишы равен 12 ("цифра 5 на доп. клавиатуре") - повернуть фигурку
+                else if (event.getKeyCode() ==  12)
+                    figure.rotate();
+                    //Если "пробел" - фигурка падает вниз на максимум
+                else if (event.getKeyCode() ==  KeyEvent.VK_SPACE)
+                    figure.downMaximum();
+            }
+
+            step();             //делаем очередной шаг
+            field.print();      //печатаем состояние "поля"
+            Thread.sleep(300);  //пауза 300 миллисекунд - 1/3 секунды
+        }
+
+        //Выводим сообщение "Game Over"
+        System.out.println("Game Over");
     }
 
-    public void step() {
+    /**
+     * Один шаг игры
+     */
+    public void step()
+    { //опускам фигурку вниз
+        figure.down();
+        //если разместить фигурку на текущем месте невозможно
+        if (!figure.isCurrentPositionAvailable())
+        {
+            figure.up();                    //поднимаем обратно
+            figure.landed();                //приземляем
 
+            isGameOver = figure.getY() <= 1;//если фигурка приземлилась на самом верху - игра окончена
+
+            field.removeFullLines();        //удаляем заполненные линии
+
+            figure = FigureFactory.createRandomFigure(field.getWidth() / 2, 0);
+
+        }
     }
 
-    public static void main(String[] args) {
-        Tetris tetris = new Tetris();
+    public static void main(String[] args) throws Exception {
+        Tetris tetris = new Tetris(10, 20);
         game = tetris;
         game.run();
     }
