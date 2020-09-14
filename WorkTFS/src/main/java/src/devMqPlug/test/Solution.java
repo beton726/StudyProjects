18b0d50a8b34f120e4a4a042f6cf5561ecd69594
beton726 <909043377fas>
2020-09-14 22:20:57 +0300

    Задача Агрегатор

--- WorkTFS/src/main/java/src/devMqPlug/test/Solution.java
+++ WorkTFS/src/main/java/src/devMqPlug/test/Solution.java
@@ -1,7 +1,12 @@
 package src.devMqPlug.test;
 
+import src.devMqPlug.listener.TFS_Listener;
+
 public class Solution {
     public static void main(String[] args) {
+
+        TFS_Listener tfs_listener = new TFS_Listener();
+
 
     }
 }
