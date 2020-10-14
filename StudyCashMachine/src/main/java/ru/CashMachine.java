61408629bd8a8986a87d4ab33bcb8246df119d6f
beton726 <909043377fas>
2020-10-15 00:14:27 +0300

    Новая задача Банкомат

--- StudyCashMachine/src/main/java/ru/CashMachine.java
+++ StudyCashMachine/src/main/java/ru/CashMachine.java
@@ -1,7 +1,11 @@
 package ru;
 
+import ru.output.ConsoleHelper;
+
 public class CashMachine {
     public static void main(String[] args) {
+        ConsoleHelper consoleHelper = new ConsoleHelper();
 
+        consoleHelper.getValidTwoDigits(consoleHelper.askCurrencyCode());
     }
 }
--- StudyCashMachine/src/main/java/ru/output/ConsoleHelper.java
+++ StudyCashMachine/src/main/java/ru/output/ConsoleHelper.java
@@ -20,4 +20,25 @@ public class ConsoleHelper {
         }
         return null;
     }
+    // Считаывется код валюты
+    public String askCurrencyCode() {
+        while(true) {
+            System.out.println("Введите валюту.");
+            String valute = readString();
+            if(valute.length() == 3)
+                return valute.toUpperCase();
+            else
+                System.out.println("Введены не корректные данные.");
+        }
+    }
+    // Считывается номинал и количество банкнот
+    public String [] getValidTwoDigits(String currencyCode) {
+        System.out.println("Введите два целых числа.");
+//        String par = readString(); // Номинал
+//        String countMoney = readString(); // Количество банкнот
+        String[] numb = readString().split(" ");
+        // Как то обработать ввод чисел
+        System.out.println(numb[0] + " " + numb[1]);
+        return null;
+    }
 }
